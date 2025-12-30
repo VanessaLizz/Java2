ed91e6dd09e88463a12be0e26de925f5def4af33
Vanessa Liz <vanessalizsousa1996@gmail.com>
2025-12-30 16:42:38 -0300

    atualizações no hotel

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,5 +1,10 @@
 public class Hotel {
     public String reservaDequarto;
+    public int numeroQuarto;
+    public String nomeHospede;
+    public boolean ocupacao;
+    public int NdeDiasReservados;
+
 
 
     public String reservarQuarto(){
